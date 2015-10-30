08700e98b871a93f43293c56dc34a739ce379611
lipengju <wangpingl@rayootech.com>
2015-10-30 13:30:19 +0800

    对代码进行了函数式的重构

--- allTests.py
+++ allTests.py
@@ -4,18 +4,18 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-import unittest
-import HTMLTestRunner
-import time
-import  os
+import time,os,smtplib,unittest,HTMLTestRunner,datetime
 from  email.mime.text import MIMEText
 from email.mime.multipart import  MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email import encoders
 from email.header import  Header
 from email.utils import  parseaddr,formataddr
-import smtplib
 
+#测试用例的目录
+listTestCase='D:/git/GITHUB/selenium-appium/TestCase'
+#测试报告的目录
+result_dir='D:/git/GITHUB/selenium-appium/Test-report/'
 
 #发送邮件
 def sentMail(newFile,from_addr='SINA email account',passwd='SINA email account password',smtp_server='smtp.sina.com',to_addr='QQ mailbox'):
@@ -31,7 +31,7 @@ def sentMail(newFile,from_addr='SINA email account',passwd='SINA email account p
     #定义发送时间
     msg['date']=time.strftime('%a,%d %b %Y %H:%M:%S %z')
     server=smtplib.SMTP(smtp_server,25)
-    # server.set_debuglevel(1)
+    server.set_debuglevel(1)
     server.login(from_addr,passwd)
     server.sendmail(from_addr,[to_addr],msg.as_string())
     server.quit()
@@ -39,7 +39,6 @@ def sentMail(newFile,from_addr='SINA email account',passwd='SINA email account p
 
 #找到最新的测试报告
 def sendReport():
-    result_dir='D:/git/PyCharm/SeleniumHq/Test-report'
     lists=os.listdir(result_dir)
     lists.sort(key=lambda fn: os.path.getmtime(result_dir+"\\"+fn) if not os.path.isdir(result_dir+"\\"+fn) else 0)
     print u'最新测试报告:',lists[-1]
@@ -52,7 +51,6 @@ def sendReport():
 
 
 
-listTestCase='D:\\git\\PyCharm\\SeleniumHq\\TestCase'
 
 def createSuite():
 	testunit=unittest.TestSuite()
@@ -71,39 +69,26 @@ def createSuite():
 			print testunit
 	return testunit
 
-allTestNames=createSuite()
 #获取当前时间
-now=time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+def getNowTime():
+	return time.strftime("%Y-%m-%d %H_%M_%S",time.localtime(time.time()))
+
 
 #自动化测试报告
-filename='Test-report/'+now+'Report.html'
-fp=file(filename,'wb')
-runner=HTMLTestRunner.HTMLTestRunner(
-	stream=fp,
-	title=u'SELENIUM WEBDRIVER AUTOMATION WEB TEST REPORT',
-	description=u'AUTOMATION WEB TEST REPORT'
-)
+def runnerAutomationTest():
+	filename=result_dir+getNowTime()+'Report.html'
+	fp=file(filename,'wb')
+	runner=HTMLTestRunner.HTMLTestRunner(
+		stream=fp,
+		title=u'SELENIUM WEBDRIVER AUTOMATION WEB TEST REPORT',
+		description=u'AUTOMATION WEB TEST REPORT'
+	)
+	runner.run(createSuite())
+	fp.close()
+	sendReport()
+	sys.exit(1)
 
-runner.run(allTestNames)
-fp.close()
-sendReport()
-sys.exit()
-
-#控制脚本执行的时间,注意：设置的时间必须大于当前的时间
-# k=1
-# while k<2:
-# 	timeing=time.strftime('%H_%M',time.localtime())
-# 	if timeing=='12_50':
-# 		print u'Start test execution scripts'
-# 		runner.run(allTestNames)
-# 		print u'Test scripts finish to exit'
-# 		break
-# 	else:
-# 		time.sleep(5)
-# 		print timeing
-# fp.close()
-# sendReport()
-
-
+if __name__=='__main__':
+	runnerAutomationTest()
 
 
